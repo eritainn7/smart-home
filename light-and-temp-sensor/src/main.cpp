46b2c4103d003ed528c939038a1120eb2d63b808
s0169537 <sanyapridava@mail.ru>
2026-07-01 16:29:00 +0300

    feat given lux and temp from esp32

---
+++ light-and-temp-sensor/src/main.cpp
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <BH1750.h>
+#include <LM75.h>
+
+LM75 tempSensor;
+BH1750 lightMeter;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  
+  if (lightMeter.begin()) {
+    Serial.println("Датчик BH1750 инициализирован успешно.");
+  } else {
+    Serial.println("Ошибка инициализации датчика!");
+  }
+
+  Wire.beginTransmission(LM75_ADDRESS);
+  if (Wire.endTransmission() == 0) {
+    Serial.println("LM75A найден на шине I2C.");
+    Serial.print("Конфигурация LM75: 0x");
+    Serial.println(tempSensor.conf(), HEX);
+  } else {
+    Serial.println("LM75A не обнаружен! Проверьте подключение.");
+  }
+
+  Serial.println("==============================");
+}
+
+void loop() {
+  float lux = lightMeter.readLightLevel();
+  float temperature = tempSensor.temp();
+  
+  Serial.print("Освещенность: ");
+  Serial.print(lux);
+  Serial.print(" lx —————————————");
+
+  Serial.print("Температура: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  delay(1000);
+}
